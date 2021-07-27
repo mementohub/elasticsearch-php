921e1e87003f611241d2215cf304506217b454bf
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2021-07-27 03:27:48 +0300

    fixed some php 8 related errors

--- src/Elasticsearch/Connections/Connection.php
+++ src/Elasticsearch/Connections/Connection.php
@@ -311,13 +311,18 @@ class Connection implements ConnectionInterface
     private function getURI($uri, $params)
     {
         if (isset($params) === true && !empty($params)) {
-            array_walk($params, function (&$value, &$key) {
-                if ($value === true) {
-                    $value = 'true';
-                } elseif ($value === false) {
-                    $value = 'false';
-                }
-            });
+            $params = array_map(
+                function ($value) {
+                    if ($value === true) {
+                        return 'true';
+                    } elseif ($value === false) {
+                        return 'false';
+                    }
+
+                    return $value;
+                },
+                $params
+            );
 
             $uri .= '?' . http_build_query($params);
         }
